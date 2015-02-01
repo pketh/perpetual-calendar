f32aa452b5daacd917e8bfa6b9d3116866f85e13
Pirijan ☃ <pirijan@gmail.com>
2015-01-31 23:06:07 -0500

    add color changing and shuffling logic

--- js/main.js
+++ js/main.js
@@ -4,15 +4,41 @@ $(function() {
     month = moment().format('MMM'),
     num = moment().format('D');
 
-  colors = randomColor({count: 12});
-  // ..
+  colorPanels();
 
-  $('.day').text(day)
-  $('.month').text(month)
-  $('.num').text(num)
+  function colorPanels() {
+    var colors = randomColor({count: 6}); //luminosity: 'light',
+    colors.forEach(function(color, index) {
+      var panel = '.panel' + (index + 1) + ' path';
+      $(panel)[0].setAttribute('fill', color);
+    })
+    reorderPanels();
+  }
 
-  $('.refresh').click(function() {
-    console.log('( ´ ▽ ` )ﾉ');
+  function reorderPanels() {
+    // console.log('change the order of panels')
+    var panels = $('svg');
+    var shuffledPanels = knuthShuffle(panels);
+
+
+    panels.remove();
+
+    shuffledPanels.each(function(index, panel) {
+      // console.log(element)
+      $('figure').prepend(panel)
+    })
+    // shuffledPanels.toArray.forEach(function(panel, index) {
+    //   console.log(panel);
+    // })
+    // shuffle array, remove figure contents, add figure contents (array)
+  }
+
+  $('.day').text(day);
+  $('.month').text(month);
+  $('.num').text(num);
+
+  $('.refresh, figure').click(function() {
+    colorPanels();
   })
 
 });
